aeb93a0f4523a3de91c81b3fcefede1e9deef9d5
Luca Giovenzana <luca@giovenzana.org>
2021-05-07 10:01:48 +0200

    [refactor] extract function that returns advantage or winner

--- python/tennis.py
+++ python/tennis.py
@@ -3,31 +3,23 @@
 class TennisGame1:
 
     def __init__(self, player_1_name, player_2_name):
-        self.player1Name = player_1_name
-        self.player2Name = player_2_name
+        self.player_1_name = player_1_name
+        self.player_2_name = player_2_name
         self.p1points = 0
         self.p2points = 0
 
     def won_point(self, player_name):
-        if player_name == self.player1Name:
+        if player_name == self.player_1_name:
             self.p1points += 1
         else:
             self.p2points += 1
 
     def score(self):
-        result = ""
         if self.p1points == self.p2points:
             result = self._convert_tie_points_to_tennis_score(self.p1points)
         elif self.p1points >= 4 or self.p2points >= 4:
-            minus_result = self.p1points - self.p2points
-            if minus_result == 1:
-                result = "Advantage " + self.player1Name
-            elif minus_result == -1:
-                result = "Advantage " + self.player2Name
-            elif minus_result >= 2:
-                result = "Win for " + self.player1Name
-            else:
-                result = "Win for " + self.player2Name
+            result = self._return_advantage_or_winner(self.p1points, self.p2points,
+                                                      self.player_1_name, self.player_2_name)
         else:
             result = self._covert_intermediate_points_to_tennis_score(self.p1points, self.p2points)
         return result
@@ -55,4 +47,18 @@ class TennisGame1:
                 2: "Thirty",
                 3: "Forty",
             }[temp_score]
+        return result
+
+    @staticmethod
+    def _return_advantage_or_winner(p1points, p2points, player_1_name, player_2_name):
+        minus_result = p1points - p2points
+
+        if minus_result == 1:
+            result = "Advantage " + player_1_name
+        elif minus_result == -1:
+            result = "Advantage " + player_2_name
+        elif minus_result >= 2:
+            result = "Win for " + player_1_name
+        else:
+            result = "Win for " + player_2_name
         return result
