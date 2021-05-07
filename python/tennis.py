ff04b22501a7637779a06a7550f7aebbf0b5ea4a
Luca Giovenzana <luca@giovenzana.org>
2021-05-07 09:32:53 +0200

    [refactor] ide safe refactor and pep8

--- python/tennis.py
+++ python/tennis.py
@@ -2,21 +2,20 @@
 
 class TennisGame1:
 
-    def __init__(self, player1Name, player2Name):
-        self.player1Name = player1Name
-        self.player2Name = player2Name
+    def __init__(self, player_1_name, player_2_name):
+        self.player1Name = player_1_name
+        self.player2Name = player_2_name
         self.p1points = 0
         self.p2points = 0
 
-    def won_point(self, playerName):
-        if playerName == self.player1Name:
+    def won_point(self, player_name):
+        if player_name == self.player1Name:
             self.p1points += 1
         else:
             self.p2points += 1
 
     def score(self):
         result = ""
-        tempScore = 0
         if self.p1points == self.p2points:
             result = {
                 0: "Love-All",
@@ -24,26 +23,26 @@ class TennisGame1:
                 2: "Thirty-All",
             }.get(self.p1points, "Deuce")
         elif self.p1points >= 4 or self.p2points >= 4:
-            minusResult = self.p1points - self.p2points
-            if (minusResult == 1):
+            minus_result = self.p1points - self.p2points
+            if minus_result == 1:
                 result = "Advantage " + self.player1Name
-            elif (minusResult == -1):
+            elif minus_result == -1:
                 result = "Advantage " + self.player2Name
-            elif (minusResult >= 2):
+            elif minus_result >= 2:
                 result = "Win for " + self.player1Name
             else:
                 result = "Win for " + self.player2Name
         else:
             for i in range(1, 3):
                 if i == 1:
-                    tempScore = self.p1points
+                    temp_score = self.p1points
                 else:
                     result += "-"
-                    tempScore = self.p2points
+                    temp_score = self.p2points
                 result += {
                     0: "Love",
                     1: "Fifteen",
                     2: "Thirty",
                     3: "Forty",
-                }[tempScore]
+                }[temp_score]
         return result
