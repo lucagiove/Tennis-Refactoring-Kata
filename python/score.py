40b16e20f6cda52a96a0383849acf6809e35f7c1
Luca Giovenzana <luca@giovenzana.org>
2021-05-20 10:38:21 +0200

    [refactor] starts replace conditional with polymorphism

---
+++ python/score.py
@@ -0,0 +1,55 @@
+
+class Score:
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.player_in_advantage = None
+
+    def compute_score(self):
+        score_data = {}
+
+        if self.player_1.points > self.player_2.points:
+            score_data['player_in_advantage'] = self.player_1.name
+        else:
+            score_data['player_in_advantage'] = self.player_2.name
+
+        if self.player_1.points >= 4 or self.player_2.points >= 4:
+            score_data['advantage_mode'] = True
+            if abs(self.player_1.points - self.player_2.points) >= 2:
+                score_data['game_over'] = True
+
+        return score_data
+
+    def print_score(self, score_data):
+        if score_data.get('game_over'):
+            return f"Win for {score_data['player_in_advantage']}"
+        if score_data.get('advantage_mode') \
+                and score_data.get('player_in_advantage'):
+            return f"Advantage {score_data['player_in_advantage']}"
+
+        return f'{self._convert_tennis_points(self.player_1.points)}-' \
+               f'{self._convert_tennis_points(self.player_2.points)}'
+
+
+    @staticmethod
+    def _convert_tennis_points(points):
+        return {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+        }[points]
+
+
+class TieScore(Score):
+    def __init__(self, player_1, player_2):
+        super().__init__(player_1, player_2)
+
+    def print_score(self, score_data):
+        return self._convert_tie_points(self.player_1.points)
+
+    def _convert_tie_points(self, points):
+        if points < 3:
+            return f"{self._convert_tennis_points(points)}-All"
+        else:
+            return "Deuce"
--- python/tennis.py
+++ python/tennis.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+from score import Score, TieScore
+
 
 class TennisGame1:
 
     def __init__(self, player_1_name, player_2_name):
         self.player_1 = Player(player_1_name)
         self.player_2 = Player(player_2_name)
-        self._score = Score(self.player_1, self.player_2)
+        self.current_score = None
 
     def won_point(self, player_name):
         if player_name == self.player_1.name:
@@ -13,9 +15,18 @@ class TennisGame1:
         else:
             self.player_2.won_point()
 
+    def _create_score(self):
+        if self.player_1.points == self.player_2.points:
+            return TieScore(self.player_1, self.player_2)
+        return Score(self.player_1, self.player_2)
+
     def score(self):
-        score_data = self._score._compute_score()
-        return self._score._print_score(score_data)
+        self.current_score = self._create_score()
+        score_data = self.current_score.compute_score()
+        return self.current_score.print_score(score_data)
+
+
+
 
 
 class Player:
@@ -26,52 +37,3 @@ class Player:
     def won_point(self):
         self.points += 1
 
-
-class Score:
-    def __init__(self, player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
-
-    def _compute_score(self):
-        score_data = {}
-
-        if self.player_1.points == self.player_2.points:
-            score_data['player_in_advantage'] = None
-        elif self.player_1.points > self.player_2.points:
-            score_data['player_in_advantage'] = self.player_1.name
-        else:
-            score_data['player_in_advantage'] = self.player_2.name
-
-        if self.player_1.points >= 4 or self.player_2.points >= 4:
-            score_data['advantage_mode'] = True
-            if abs(self.player_1.points - self.player_2.points) >= 2:
-                score_data['game_over'] = True
-
-        return score_data
-
-    def _print_score(self, score_data):
-        if score_data.get('game_over'):
-            return f"Win for {score_data['player_in_advantage']}"
-        if score_data.get('advantage_mode') \
-                and score_data.get('player_in_advantage'):
-            return f"Advantage {score_data['player_in_advantage']}"
-
-        if not score_data.get('player_in_advantage'):
-            return self._convert_tie_points(self.player_1.points)
-        return f'{self._convert_tennis_points(self.player_1.points)}-' \
-               f'{self._convert_tennis_points(self.player_2.points)}'
-
-    def _convert_tie_points(self, points):
-        if points < 3:
-            return f"{self._convert_tennis_points(points)}-All"
-        else:
-            return "Deuce"
-
-    @staticmethod
-    def _convert_tennis_points(points):
-        return {
-            0: "Love",
-            1: "Fifteen",
-            2: "Thirty",
-            3: "Forty",
-        }[points]
