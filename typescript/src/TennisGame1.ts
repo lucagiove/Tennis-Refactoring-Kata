3b9a99b0a587ba0f2d944232781c45c82676a0a9
Luca Giovenzana <luca@giovenzana.org>
2022-03-25 23:07:02 +0100

    refactor(game1): first refactor 2 pomodori

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,71 +1,90 @@
-import { TennisGame } from './TennisGame';
+import {TennisGame} from './TennisGame';
 
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
+    private player1Score: number = 0;
+    private player2Score: number = 0;
+    private player1Name: string;
+    private player2Name: string;
 
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
-
-  wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1;
-    else
-      this.m_score2 += 1;
-  }
-
-  getScore(): string {
-    let score: string = '';
-    let tempScore: number = 0;
-    if (this.m_score1 === this.m_score2) {
-      switch (this.m_score1) {
-        case 0:
-          score = 'Love-All';
-          break;
-        case 1:
-          score = 'Fifteen-All';
-          break;
-        case 2:
-          score = 'Thirty-All';
-          break;
-        default:
-          score = 'Deuce';
-          break;
-
-      }
+    constructor(player1Name: string, player2Name: string) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
+
+    wonPoint(playerName: string): void {
+        if (playerName === 'player1')
+            this.player1Score += 1;
+        else
+            this.player2Score += 1;
     }
-    else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else { score += '-'; tempScore = this.m_score2; }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
+
+    getScore(): string {
+        if (!this.playerInAdvantage())
+            return this.tieScoreNameFor(this.player1Score);
+
+        if (this.gameIsAdvantage())
+            return `Advantage ${this.playerInAdvantage()}`;
+
+        if (this.gameIsOver())
+                return `Win for ${this.playerInAdvantage()}`;
+
+        return `${this.scoreNameFor(this.player1Score)}-${this.scoreNameFor(this.player2Score)}`
+
+    }
+
+    private gameIsAdvantage() {
+        return (this.scoreDifference() === 1) && (this.player1Score >= 4 || this.player2Score >= 4) ;
+    }
+
+    private gameIsOver() {
+        return (this.scoreDifference() >= 2) && (this.player1Score >= 4 || this.player2Score >= 4) ;
+    }
+
+    private scoreDifference() {
+        return Math.abs((this.player1Score - this.player2Score));
+    }
+
+    private playerInAdvantage(): string | null {
+        if (this.player1Score > this.player2Score)
+            return this.player1Name
+        if (this.player1Score < this.player2Score)
+            return this.player2Name
+        return null
+    }
+
+    private scoreNameFor(score: number) {
+        switch (score) {
+            case 0:
+                return 'Love';
+            case 1:
+                return 'Fifteen';
+            case 2:
+                return 'Thirty';
+            case 3:
+                return'Forty';
+            default:
+                throw new Error("Score name unknown")
         }
-      }
     }
-    return score;
-  }
+
+    private tieScoreNameFor(score: number) {
+        let result: string
+        switch (score) {
+            case 0:
+                result = 'Love-All';
+                break;
+            case 1:
+                result = 'Fifteen-All';
+                break;
+            case 2:
+                result = 'Thirty-All';
+                break;
+            default:
+                result = 'Deuce';
+                break;
+
+        }
+        return result;
+    }
 }
