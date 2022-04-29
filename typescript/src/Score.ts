bafefd3c11e0d1e58a0d36172d751dedabbe5568
Luca Giovenzana <luca@giovenzana.org>
2022-04-29 02:53:34 +0200

    refactor(game1): chain of responsibility

---
+++ typescript/src/Score.ts
@@ -0,0 +1,115 @@
+class Score {
+    constructor(protected m_score1: number, protected m_score2: number) {
+    }
+
+    public canBeApplied(): boolean {
+        throw new Error('Not Implemented')
+    }
+
+    public calculate(): string {
+        throw new Error('Not Implemented')
+    }
+
+    protected scoreDifference() {
+        return this.m_score1 - this.m_score2;
+    }
+
+    protected playerInAdvantage() {
+        if (this.m_score1 > this.m_score2)
+            return "player1"
+        if (this.m_score2 > this.m_score1)
+            return "player2"
+        return null
+    }
+}
+
+export class InProgressScore extends Score {
+    constructor(m_score1: number, m_score2: number) {
+        super(m_score1, m_score2);
+    }
+
+    public canBeApplied(): boolean {
+        return (this.m_score1 !== this.m_score2) && (this.m_score1 < 4 && this.m_score2 < 4);
+    }
+
+    public calculate(): string {
+        let tempScore: number = 0;
+        let score = ''
+        for (let i = 1; i < 3; i++) {
+            if (i === 1) tempScore = this.m_score1;
+            else {
+                score += '-';
+                tempScore = this.m_score2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += 'Love';
+                    break;
+                case 1:
+                    score += 'Fifteen';
+                    break;
+                case 2:
+                    score += 'Thirty';
+                    break;
+                case 3:
+                    score += 'Forty';
+                    break;
+            }
+        }
+        return score;
+    }
+}
+
+export class TieScore extends Score {
+    constructor(m_score1: number, m_score2: number) {
+        super(m_score1, m_score2);
+    }
+
+    public canBeApplied(): boolean {
+        return this.m_score1 === this.m_score2;
+    }
+
+    public calculate(): string {
+        switch (this.m_score1) {
+            case 0:
+                return 'Love-All';
+            case 1:
+                return 'Fifteen-All';
+            case 2:
+                return 'Thirty-All';
+            default:
+                return 'Deuce';
+        }
+    }
+}
+
+export class AdvantageScore extends Score {
+    constructor(m_score1: number, m_score2: number) {
+        super(m_score1, m_score2);
+    }
+
+    public canBeApplied(): boolean {
+        return (this.m_score1 >= 4 || this.m_score2 >= 4) && (Math.abs(this.scoreDifference()) === 1);
+    }
+
+    public calculate(): string {
+        return `Advantage ${this.playerInAdvantage()}`;
+
+    }
+}
+
+export class EndScore extends Score {
+    constructor(m_score1: number, m_score2: number) {
+        super(m_score1, m_score2);
+    }
+
+    public canBeApplied(): boolean {
+        return (this.m_score1 >= 4 || this.m_score2 >= 4) && (Math.abs(this.scoreDifference()) > 1);
+    }
+
+    public calculate(): string {
+        if (this.scoreDifference() >= 2) return 'Win for player1';
+        return 'Win for player2';
+    }
+
+}
--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,4 +1,5 @@
 import {TennisGame} from './TennisGame';
+import {AdvantageScore, EndScore, InProgressScore, TieScore} from "./Score";
 
 export class TennisGame1 implements TennisGame {
     private m_score1: number = 0;
@@ -19,99 +20,16 @@ export class TennisGame1 implements TennisGame {
     }
 
     getScore(): string {
-        if (this.gameIsInProgress()){
-            return this.scoreForInProgress();
-        }
-        if (this.gameIsTie()) {
-            return this.scoreForTie();
-        }
-        if (this.gameIsAdvantage()) {
-            return this.scoreForAdvantage();
-        }
-        if (this.gameIsOver()) {
-            return this.scoreForWin();
-        }
+        const inProgressScore = new InProgressScore(this.m_score1, this.m_score2)
+        const tieScore = new TieScore(this.m_score1, this.m_score2)
+        const advantageScore = new AdvantageScore(this.m_score1, this.m_score2)
+        const gameOverScore = new EndScore(this.m_score1, this.m_score2)
+
+        const possibleScores = [inProgressScore, tieScore, advantageScore, gameOverScore]
+
+        const currentScore = possibleScores.filter((s) => s.canBeApplied())[0]
+        return currentScore.calculate()
+
         throw new Error('What game are you playing?')
     }
-
-    private gameIsInProgress() {
-        return !(this.gameIsTie() || this.gameIsOver() || this.gameIsAdvantage());
-    }
-
-    private gameIsTie() {
-        return this.m_score1 === this.m_score2;
-    }
-
-    private gameIsAdvantage() {
-        return (this.m_score1 >= 4 || this.m_score2 >= 4) && (Math.abs(this.scoreDifference()) === 1);
-    }
-
-    private gameIsOver() {
-        return (this.m_score1 >= 4 || this.m_score2 >= 4) && (Math.abs(this.scoreDifference()) > 1);
-    }
-
-
-    private scoreForAdvantage() {
-        return `Advantage ${this.playerInAdvantage()}`;
-    }
-
-    private scoreForInProgress() {
-        let tempScore: number = 0;
-        let score = ''
-        for (let i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += '-';
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += 'Love';
-                    break;
-                case 1:
-                    score += 'Fifteen';
-                    break;
-                case 2:
-                    score += 'Thirty';
-                    break;
-                case 3:
-                    score += 'Forty';
-                    break;
-            }
-        }
-        return score;
-    }
-
-    private scoreForWin() {
-        if (this.scoreDifference() >= 2) return 'Win for player1';
-        return 'Win for player2';
-    }
-
-    private playerInAdvantage() {
-        if (this.m_score1 > this.m_score2)
-            return "player1"
-        if (this.m_score2 > this.m_score1)
-            return "player2"
-        return null
-    }
-
-    private scoreDifference() {
-        return this.m_score1 - this.m_score2;
-    }
-
-
-    private scoreForTie() {
-        switch (this.m_score1) {
-            case 0:
-                return 'Love-All';
-            case 1:
-                return 'Fifteen-All';
-            case 2:
-                return 'Thirty-All';
-            default:
-                return 'Deuce';
-        }
-    }
-
-
 }
